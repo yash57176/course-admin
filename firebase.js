a09b11c0589d6a57d32dac9a6d6075ec83c04910
yashpandit657 <sharmashivani0492@gmail.com>
2026-08-16 16:50:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -29,17 +29,6 @@ import {
   uploadBytes, 
   getDownloadURL 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
-
-// Your Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDjeVzy3FXGcS7-iyCWNtIgrjEPDPFAim0",
-  authDomain: "printex-c59f6.firebaseapp.com",
-  databaseURL: "https://printex-c59f6-default-rtdb.firebaseio.com",
-  projectId: "printex-c59f6",
-  storageBucket: "printex-c59f6.firebasestorage.app",
-  messagingSenderId: "635263652250",
-  appId: "1:635263652250:web:878f4321eeffb7d1e6aa23",
-  measurementId: "G-G5DQPS1QMS"
 };
 
 // Initialize Firebase
